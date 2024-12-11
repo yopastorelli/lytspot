5b33b63a6faa79fb1e491e41623ed1d8a52ab49c
yopastorelli <dfdelgado@gmail.com>
2024-12-11 19:54:20 -0300

    a
    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
   output: 'static',
   base: '/', // Mantenha "/" se for um domínio raiz ou "/repositorio" se for um subdomínio
 });
