bfbdb719c01f853771086fbb3cc55945a9cd16c9
yopastorelli <dfdelgado@gmail.com>
2024-12-12 12:10:29 -0300

    O
    O

---
+++ node_modules/astro/dist/core/routing/dev-default.d.ts
@@ -0,0 +1,3 @@
+import type { AstroSettings, ManifestData } from '../../types/astro.js';
+import type { SSRManifest } from '../app/types.js';
+export declare function injectDefaultDevRoutes(settings: AstroSettings, ssrManifest: SSRManifest, routeManifest: ManifestData): ManifestData;
