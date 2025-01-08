eba5088a116f0786f19d44acf1f39f89c6955213
yopastorelli <dfdelgado@gmail.com>
2025-01-08 10:07:22 -0300

    11

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,10 +2,11 @@
 
 // Declaração de variáveis do ambiente
 interface ImportMetaEnv {
-    readonly BASE_URL: string; // Adicione outras variáveis conforme necessário
+    readonly BASE_URL: string; // Variável obrigatória
     readonly [key: string]: string | undefined; // Permite variáveis adicionais
   }
   
-interface ImportMeta {
+  interface ImportMeta {
     readonly env: ImportMetaEnv; // Extensão da interface para incluir "env"
-}
+  }
+  
